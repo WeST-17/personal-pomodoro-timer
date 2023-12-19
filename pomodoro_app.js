35582268d6a5b6a367e0b14392af91db94c51fc2
Wesley Tsai <wes.tsai97@gmail.com>
2023-12-19 10:02:51 -0600

    design changes, now onto features

--- pomodoro_app.js
+++ pomodoro_app.js
@@ -1,12 +1,12 @@
 const bells = new Audio('./sounds/mixkit-happy-bell.wav'); 
-const startBtn = document.getElementById('start-btn'); 
+const startBtn = document.querySelector('.start-btn'); 
 const session = document.querySelector('.minutes'); 
 let myInterval; 
 let state = true;
 
 const appTimer = () => {
     const sessionTime = Number.parseInt(session.textContent);
-
+    console.log(sessionTime);
     if(state) {
         state = false;
         let totalSeconds = sessionTime * 60;
@@ -25,7 +25,7 @@ const appTimer = () => {
             } else {
                 secondClass.textContent = secondsLeft;
             }
-            minuteClass.textContent = '${minutesLeft}';
+            minuteClass.textContent = `${minutesLeft}`;
 
             if(minutesLeft === 0 && secondsLeft === 0) {
                 bells.play();
@@ -37,5 +37,7 @@ const appTimer = () => {
     } else {
         alert('Session has already started.')
     }
-
 }
+
+// Event listener to start the timer:
+startBtn.addEventListener('click', appTimer);
